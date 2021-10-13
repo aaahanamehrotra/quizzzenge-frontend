b2563ea77e8e2dccfa50e8e17e8e7e046e4a5f50
aaahana <e10991aaahana@dpsrkp.net>
2021-10-13 19:54:21 +0530

    fetch name

--- src/components/Quizbox/Quizbox.js
+++ src/components/Quizbox/Quizbox.js
@@ -1,9 +1,17 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Quizbox = (props) => {
+    const [quizName, setquizName] = useState("")
+    useEffect(() => {
+        console.log(`http://localhost:5000/qname/${props.x}`)
+        axios.get(`http://localhost:5000/qname/${props.x}`).then(res => setquizName(res.data[0]["quizname"]))
+    }, [])
     return (
         <div>
-            <h1>quiz name</h1>
+            <h1>{quizName}</h1>
             <button value={props.x} onClick={props.onAttQ}>open</button>
         </div>
     )
